7f640ebbd008bbf702c5369ea4d6af6b1166e6ae
HanLei <triplehan@163.com>
2022-05-03 20:37:30 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
     }
 }
